d97b3382c6157ef7545a09836bb4602cc1b2e942
Mingyu-Song <sv002@naver.com>
2021-09-13 23:56:41 +0900

    [add] toast-ui editor

--- src/pages/write/index.tsx
+++ src/pages/write/index.tsx
@@ -1,9 +1,44 @@
+const Editor = dynamic(
+  () => import('@toast-ui/react-editor').then((m) => m.Editor),
+  { ssr: false }
+);
+
+// import Prism from 'prismjs';
+// import 'prismjs/themes/prism.css';
+
+// import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
+// const codeSyntaxHighlight = dynamic(
+//   () => import('@toast-ui/editor-plugin-code-syntax-highlight'),
+//   { ssr: false }
+// );
+
+// import 'tui-color-picker/dist/tui-color-picker.css';
+// import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
+// const colorSyntax = dynamic(
+//   () => import('@toast-ui/editor-plugin-color-syntax'),
+//   { ssr: false }
+// );
+
+// import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
+
+import '@toast-ui/editor/dist/toastui-editor.css';
+
 import GlobalNav from 'components/nav/GlobalNav';
+import dynamic from 'next/dynamic';
+import { useRef } from 'react';
 
 export default function Write() {
   return (
     <div>
       <GlobalNav />
+      <Editor
+        previewStyle="vertical"
+        initialValue="hello react editor world!"
+        height="600px"
+        initialEditType="markdown"
+        useCommandShortcut={true}
+        // plugins={[colorSyntax]}
+      />
     </div>
   );
 }
